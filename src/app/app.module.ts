4faf2857314339d383873127ea8ff1d91a371280
marcosjrs <marcosjrs80@gmail.com>
2018-10-31 09:01:50 +0100

    Eliminacion de componentes de fabrica: contact y home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,6 @@ import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
 import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
@@ -18,8 +16,6 @@ import { TareasService } from '../services/tareas.service';
   declarations: [
     MyApp,
     AboutPage,
-    ContactPage,
-    HomePage,
     TabsPage,
     PendientesComponent,
     TerminadosComponent
@@ -32,8 +28,6 @@ import { TareasService } from '../services/tareas.service';
   entryComponents: [
     MyApp,
     AboutPage,
-    ContactPage,
-    HomePage,
     TabsPage,
     PendientesComponent,
     TerminadosComponent
