8bc7c612dd4afe39ea756e7bf268dc004d895f8d
marcosjrs <marcosjrs80@gmail.com>
2018-10-31 11:26:26 +0100

    Recogida del titulo, pasado mediante NavCtrl, comienzando creacion Lista

--- src/pages/agregar/agregar.ts
+++ src/pages/agregar/agregar.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { TareasService } from '../../services/tareas.service';
 import { NavParams } from 'ionic-angular';
+import { Lista } from '../../models';
 
 
 @Component({
@@ -8,8 +9,10 @@ import { NavParams } from 'ionic-angular';
   templateUrl: 'agregar.html',
 })
 export class AgregarPage {
+  lista:Lista;
 
-  constructor(private tareasSvc: TareasService, private navParams: NavParams) {    
+  constructor(private tareasSvc: TareasService, private navParams: NavParams) {   
+    this.lista = new Lista( this.navParams.get('titulo') );
     console.log( this.navParams.get('titulo'));
   }
 
