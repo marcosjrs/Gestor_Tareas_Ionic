b5980a314b5c915b3129c60663c780fa6c13340b
marcosjrs <marcosjrs80@gmail.com>
2018-10-31 12:17:52 +0100

    Añadiendo tareas y mostrandolas en AgregarPage (no persistente)

--- src/models/lista.model.ts
+++ src/models/lista.model.ts
@@ -3,7 +3,7 @@ import { Tarea } from "./tarea.model";
 export class Lista{
     constructor(  public titulo:string, 
                 public creacion:Date = new Date(), public id:number = new Date().getTime(),
-                public tareas?:Tarea[],
+                public tareas:Tarea[] = [],
                 public finalizacion?:Date){
 
     }
--- src/pages/agregar/agregar.ts
+++ src/pages/agregar/agregar.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { TareasService } from '../../services/tareas.service';
 import { NavParams } from 'ionic-angular';
-import { Lista } from '../../models';
+import { Lista, Tarea } from '../../models';
 
 
 @Component({
@@ -10,10 +10,15 @@ import { Lista } from '../../models';
 })
 export class AgregarPage {
   lista:Lista;
+  descripcionNuevaTarea:string;
 
   constructor(private tareasSvc: TareasService, private navParams: NavParams) {   
     this.lista = new Lista( this.navParams.get('titulo') );
-    console.log( this.navParams.get('titulo'));
+  }
+
+  guardarNuevaTarea(){
+    this.lista.tareas.push( new Tarea(this.descripcionNuevaTarea, false) );
+    this.descripcionNuevaTarea = "";
   }
 
 }
