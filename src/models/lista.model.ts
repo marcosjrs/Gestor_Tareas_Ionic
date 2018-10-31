143af782d0b7f3f2db4d08c99a9d3f9c0d09e865
marcosjrs <marcosjrs80@gmail.com>
2018-10-31 09:21:09 +0100

    Cambio de ubicación de los models

--- src/models/lista.model.ts
+++ src/models/lista.model.ts
@@ -1,8 +1,9 @@
 import { Tarea } from "./tarea.model";
 
 export class Lista{
-    constructor(public tareas:Tarea[],  public titulo:string, 
+    constructor(  public titulo:string, 
                 public creacion:Date = new Date(), public id:number = new Date().getTime(),
+                public tareas?:Tarea[],
                 public finalizacion?:Date){
 
     }
